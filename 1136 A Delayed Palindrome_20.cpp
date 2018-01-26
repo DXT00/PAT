16f6ecae7f5f80af8fcd3fa927a2ffc81dafaa44
DXT00 <dingxiaotong11@yahoo.com>
2018-01-26 17:02:46 +0800

    Update 1136 A Delayed Palindrome_20.cpp

--- 1136 A Delayed Palindrome_20.cpp
+++ 1136 A Delayed Palindrome_20.cpp
@@ -5,21 +5,6 @@
 #include <string>
 #include <sstream>
 using namespace std;
-
-long long int str_to_int(string s){
-	stringstream ss;
-	ss<<s;
-	long long int res;
-	ss>>res;
-	return res;
-}
-string int_to_str(long long int t){
-	stringstream ss;
-	ss<<t;
-	string res;
-	ss>>res;
-	return res;
-}
 bool isPalindromic(string s){
 	int i=0,j=s.size()-1;
 	while(i<=j){
@@ -35,18 +20,15 @@ string add(string a,string b){
 	string str_sum,str_forward;
 	for (int i = 0; i <a.size(); ++i)
 	{
-	
 		sum=a[i]-'0'+b[i]-'0'+forward;
 		str_sum=sum%10+'0';
 		forward=sum/10;
 		res=str_sum+res;
-
 	}
 	if(forward!=0){
 		str_forward=forward+'0';
 		res=str_forward+res;
-	}
-	
+	}	
 	return res;
 }
 int main(int argc, char const *argv[])
@@ -55,7 +37,6 @@ int main(int argc, char const *argv[])
 	cin>>orgin;
 	result=orgin;
 	int i;
-	//cout<<add("9","5")<<endl;
 	for ( i= 0; i <10; ++i)
 	{
 		if(isPalindromic(result)){
@@ -66,15 +47,10 @@ int main(int argc, char const *argv[])
 		reverse(re.begin(),re.end());
 		result=add(re,orgin);
 		cout<<orgin<<" + "<<re<<" = "<<result<<endl;
-		orgin=result;
-		
-		
-		
+		orgin=result;	
 	}
 	if(i==10)
 		cout<<"Not found in 10 iterations."<<endl;
-	
-	
 
 	getchar();
 	system("pause");
