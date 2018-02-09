7000dbd3a43c696858c193b83ab0a3c584e93fa9
DXT00 <dingxiaotong11@yahoo.com>
2018-02-09 23:20:50 +0800

    字符串处理

--- 1001 A+B Format_20.cpp
+++ 1001 A+B Format_20.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 #include <stdio.h>
 #include <string>
@@ -8,9 +7,8 @@ long long a, b;
 string res, s;
 int main(int argc, char const *argv[])
 {
-	scanf_s("%lld%lld", &a, &b);
-	long long c = a + b;
-	s = to_string(c);
+	scanf("%lld%lld", &a, &b);
+	s = to_string(a + b);
 	for (int i = s.size() - 1, cnt = 1; i >= 0; i--, cnt++){
 		res = s[i] + res;
 		if (cnt % 3 == 0 && i != 0 && s[i - 1] != '-')
