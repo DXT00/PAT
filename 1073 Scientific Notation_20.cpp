8788a4789159acb9c6352c92ff83ca2b2a2c962d
DXT00 <dingxiaotong11@yahoo.com>
2018-02-20 15:02:06 +0800

    字符串处理，好题~再做3

--- 1073 Scientific Notation_20.cpp
+++ 1073 Scientific Notation_20.cpp
@@ -2,66 +2,39 @@
 一定要用string处理，否则指数为9999时，若用double会溢出！！
 
 */
-
 #include <iostream>
 #include <vector>
 #include <stdio.h>
 #include <algorithm>
 #include <string>
-#include <iomanip>
-
+#include <math.h>
 using namespace std;
-
-string s, e_after, point_before, point_after, result;
-double d_sub;
-int d_expp;
+string s,res;
+bool neg = false;
+string cof, ex;
 int main(int argc, char const *argv[])
 {
 	cin >> s;
-	bool neg = false;
 	if (s[0] == '-')neg = true;
-	int i = 1, k = 0;
-	bool behind = false;
-	while (s[i] != '.'){
-		point_before += s[i];
-		i++;
+	for (int i = 1; i <s.size(); ++i){
+		if (s[i] == 'E'){
+			ex = s.substr(i + 1);
+			break;
+		}
+		cof += s[i];
 	}
-	i++;
-	while (s[i] != 'E'){
-		k++;
-		point_after += s[i];
-		i++;
+	if (stoi(ex) < 0){
+		res = "0." + string(-stoi(ex) - 1, '0') + cof[0] + cof.substr(2);
+	}
+	else{
+		int wei = cof.substr(2).size();
+		if (wei > stoi(ex))
+			res = cof[0] + cof.substr(2, stoi(ex)) + '.' + cof.substr(2 + stoi(ex));
+		else
+			res = cof[0] + cof.substr(2) + string(stoi(ex) - wei, '0');
 	}	
-	i++;
-	while (s[i] != '\0'){
-		e_after += s[i];
-		i++;
-	}
-	d_expp = stoi(e_after);//最大9999
-
-	if (d_expp < 0){
-
-		string s(-d_expp - 1, '0');//string 初始化
-		result = "0." + s + point_before + point_after;
-	}
-	else if (d_expp > 0){
-		if (k <= d_expp){
-			
-			string s(d_expp - k, '0');
-			result = point_before + point_after + s;
-		}
-		else{
-			result = point_before + point_after.substr(0, d_expp) + '.' + point_after.substr(d_expp);
-		}
-	}
-
-	if (neg == true){
-		cout << '-';
-		cout << result << endl;
-	}
-	else
-		cout << result << endl;
-	
+	if (neg)res = '-' + res;
+	cout << res << endl;
 	system("pause");
 	return 0;
 }
