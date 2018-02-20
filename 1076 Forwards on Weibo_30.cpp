3364c310201cfaae96a6e286de3a7cbdecd16515
DXT00 <dingxiaotong11@yahoo.com>
2018-02-20 19:33:01 +0800

    bfs经典题目！dfs会超时~再做3

--- 1076 Forwards on Weibo_30.cpp
+++ 1076 Forwards on Weibo_30.cpp
@@ -1,71 +1,57 @@
+/*
+用dfs的话要用set,而且会超时，所以还是用bfs！！
+*/
 #include <iostream>
 #include <vector>
 #include <stdio.h>
-#include <queue>
 #include <algorithm>
-
+#include <queue>
 using namespace std;
-int N, L, k, x, qr, query;
-int find(vector<vector<int> > &v, int qr){
-	std::vector<bool> visited(N + 1, false);
-	visited[qr] = true;
+int N, L, k, x, Q, num;
+vector<int> v[1005];
+void bfs(int y){
+	int level[1005];
+	bool visited[1005];
+	fill(visited,visited+1005,false);
+	int x=y,cnt=0;;
+	level[x]=0;
+	visited[x]=true;
 	queue<int> q;
-	int cnt = 0;
-	int res = 0;
-	int cur;
-	int tail;
-	for (int i = 0; i <v[qr].size(); ++i)
-	{
-		if (visited[v[qr][i]] == false){
-			visited[v[qr][i]] = true;			
-			q.push(v[qr][i]);
-			tail = v[qr][i];
+	q.push(x);
+	while(!q.empty()){
+		int w=q.front();
+		if(level[w]<=L)cnt++;
+		//else break;
+		q.pop();
+		for (int i = 0; i <v[w].size(); ++i)
+		{
+			int j=v[w][i];
+			if(visited[j]==false){
+				visited[j]=true;
+				level[j]=level[w]+1;
+				q.push(j);
+			}
 		}
 	}
-	while (cnt<L&&!q.empty()){
-		int w = q.front();
-		q.pop();
-		res++;
+	cout<<cnt-1<<endl;
 
-			for (int i = 0; i <v[w].size(); ++i)
-			{
-				if (visited[v[w][i]] == false){
-					visited[v[w][i]] = true;
-					
-					q.push(v[w][i]);
-					//if (w==tail)
-						cur= v[w][i];
-				}
-				
-			}
-			if (w == tail){   /*经典求bfs层数cnt模型！！*/
-				tail = cur;
-				cnt++;
-			}
-	}
-	return res;
 }
 int main(int argc, char const *argv[])
 {
 	scanf("%d%d", &N, &L);
-	std::vector<int> cur(N + 1, 0);
-	std::vector<vector<int> > v(N + 1);//只记录入度
-	for (int i = 1; i <= N; ++i)
-	{
+
+	for (int i = 1; i <= N; ++i){
 		scanf("%d", &k);
-		for (int j = 0; j <k; ++j)
-		{
+		for (int j = 0; j <k; ++j){
 			scanf("%d", &x);
 			v[x].push_back(i);
 		}
 	}
-	scanf("%d", &query);
-	for (int i = 0; i <query; ++i)
-	{
-		scanf("%d", &qr);
-		cout << find(v, qr) << endl;
+	scanf("%d", &Q);
+	for (int i = 0; i <Q; ++i){
+		scanf("%d", &x);
+		bfs(x);
 	}
-	getchar();
 	system("pause");
 	return 0;
 }
