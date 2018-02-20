7b74e175c5e21d8165a6176905ecd475f31adb61
DXT00 <dingxiaotong11@yahoo.com>
2018-02-20 20:16:41 +0800

    排序

--- 1075 PAT Judge_25.cpp
+++ 1075 PAT Judge_25.cpp
@@ -1,86 +1,65 @@
-
 #include <iostream>
 #include <vector>
-#include <stdio.h>
 #include <algorithm>
-using namespace std;
-int N,K,M,id,pro,sc,pre_sc;
-std::vector<int> problem;
+#include <stdio.h>
 
-struct Node
-{
-	int id,sum,num;//num记录通过的题数
-	bool pass;//记录是否有一题通过
-	std::vector<int> score;//记录每题分数
-	std::vector<int> full;//记录该题是否满分
-	std::vector<bool> visited;//记录是否提交过该题
-	Node():id(-1),sum(0),num(0),pass(false){
-		score.assign(6,0);
-		full.assign(6,0);
-		visited.assign(6,false);
+using namespace std;
+struct Node{
+	int id,perfect,sum,rank;
+	vector<int>score;
+	bool submit;
+	Node():submit(false),perfect(0),sum(0){
+		score.assign(6,-2);//-2表示没提交！
 	}
 };
-std::vector<Node> v;
+int N,K,M,x,y,z;
+int full[6];
+vector<Node> v(10005),res;
 bool cmp(Node a,Node b){
-	if(a.sum!=b.sum)
-		return a.sum>b.sum;
+	if(a.sum!=b.sum)return a.sum>b.sum;
 	else{
-		if(a.num!=b.num) return a.num>b.num;
-		else{
+		if(a.perfect!=b.perfect)return a.perfect>b.perfect;
+		else
 			return a.id<b.id;
-		}
 	}
 }
 int main(int argc, char const *argv[])
-{	scanf("%d%d%d",&N,&K,&M);
-	problem.assign(K+1,0);
-	v.assign(N+1,Node());
-	int curs;
+{
+	scanf("%d%d%d",&N,&K,&M);
 	for (int i = 1; i <=K; ++i)
-		scanf("%d",&problem[i]);
-	for (int i = 0; i <M; ++i)
-	{
-		scanf("%d%d%d",&id,&pro,&sc);
-		
-		v[id].id=id;
-		v[id].visited[pro]=true;
-		pre_sc=v[id].score[pro];
-		v[id].sum-=pre_sc;
-		if(sc!=-1){v[id].pass=true;}
-		if(sc==-1){
-			curs=0;
-		}
-		else
-			curs=sc;
-		
-		v[id].score[pro]=max(v[id].score[pro],curs);
-		if(v[id].score[pro]==problem[pro]&&v[id].full[pro]==0){v[id].num++;v[id].full[pro]++;}
-		v[id].sum+=v[id].score[pro];
+		scanf("%d",&full[i]);
+	for (int i = 0; i <M; ++i){
+		scanf("%d%d%d",&x,&y,&z);
+		v[x].id=x;
+		if(z>=0)v[x].submit=true;//>=0说明提交过编译通过~可以rank
+		v[x].score[y]=max(v[x].score[y],z);
 	}
-	sort(v.begin(),v.end(),cmp);
-	int rank=1;
-	int cnt=0;
-	int pre_sum=v[0].sum;
-	for (int i = 0; i <v.size(); ++i)
-	{
-		if(v[i].id!=-1&&v[i].pass==true){
-			cnt++;
-			if(v[i].sum==pre_sum)
-				printf("%d %05d %d",rank,v[i].id,v[i].sum);
-			else{
-				printf("%d %05d %d",cnt,v[i].id,v[i].sum);
-				rank=cnt;
+	for (int i = 1; i <=N; ++i){//计算sum perfect
+		if(v[i].submit){
+			for (int j = 1; j <=K; ++j){
+				v[i].sum+=(v[i].score[j]<0)?0:v[i].score[j];
+				if(v[i].score[j]==full[j])v[i].perfect++;
 			}
-			for (int j = 1; j <=K; ++j)
-			{
-				if(v[i].visited[j]==true)printf(" %d",v[i].score[j]);
-				else printf(" -");
-			}
-			pre_sum=v[i].sum;
-			printf("\n");
+			res.push_back(v[i]);
 		}
 	}
-	getchar();
+	sort(res.begin(), res.end(),cmp);
+	int cur;
+	for (int i = 0; i <res.size(); ++i){
+		if(i==0)res[i].rank=1;
+		else{
+			if(res[i].sum==res[i-1].sum)res[i].rank=res[i-1].rank;
+			else res[i].rank=i+1;
+		}
+		printf("%d %05d %d",res[i].rank,res[i].id,res[i].sum);
+		for (int j = 1; j <=K; ++j)
+		{
+			if(res[i].score[j]==-2)printf(" -");
+			else if(res[i].score[j]==-1)printf(" 0");
+			else printf(" %d",res[i].score[j] );
+		}
+		printf("\n");
+	}
 	system("pause");
 	return 0;
 }
