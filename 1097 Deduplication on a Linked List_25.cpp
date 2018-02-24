ead855b2a9076a3d772e067eec3f99217813d750
DXT00 <dingxiaotong11@yahoo.com>
2018-02-24 11:34:47 +0800

    静态链表

--- 1097 Deduplication on a Linked List_25.cpp
+++ 1097 Deduplication on a Linked List_25.cpp
@@ -1,53 +1,48 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
-#include<algorithm>
+#include <algorithm>
+#include <map>
 using namespace std;
-int start, N, addd, keyy, nextt, cur;
+int start,N,x,y,z;
 struct Node
 {
-	int add, key, next;
-	Node(int add, int key, int next) :add(add), key(key), next(next){}
+	int add,data,next;	
 };
+std::vector<Node> v(100002);
+vector<Node> res,remain,del;
+std::map<int, int> m;
 int main(int argc, char const *argv[])
 {
-	scanf("%d%d", &start, &N);
-	std::vector<Node> origin(100002, Node(0, 0, 0));
-	std::vector<int> m(100001, 0);
-	std::vector<Node> v;
-	std::vector<Node> remain;
-	for (int i = 0; i <N; ++i)
-	{
-		scanf("%d%d%d", &addd, &keyy, &nextt);
-		origin[addd].add = addd;
-		origin[addd].key = keyy;
-		origin[addd].next = nextt;
+	scanf("%d%d",&start,&N);
+	for (int i = 0; i <N; ++i){
+		scanf("%d%d%d",&x,&y,&z);
+		v[x].add=x;
+		v[x].data=y;
+		v[x].next=z;
 	}
-	cur = start;
-	for (int i = 0; cur!=-1&&i <N; ++i)//不要落了cur!=-1!!!!
-	{
-		m[abs(origin[cur].key)]++;
-		if (m[abs(origin[cur].key)]>1)
-			remain.push_back(origin[cur]);
-		else
-			v.push_back(origin[cur]);
-		cur = origin[cur].next;
+	int cur=start;
+	while(cur!=-1){
+		res.push_back(v[cur]);
+		cur=v[cur].next;
 	}
+	for (int i = 0; i <res.size(); ++i){
+		m[abs(res[i].data)]++;
+		if(m[abs(res[i].data)]==1)remain.push_back(res[i]);
+		else if(m[abs(res[i].data)]>1){
+			del.push_back(res[i]);		
+		}
+	}	
+	for (int i = 0; i+1 < remain.size(); ++i)
+		printf("%05d %d %05d\n",remain[i].add,remain[i].data,remain[i+1].add );
+	if(remain.size()>0)
+		printf("%05d %d %d\n",remain[remain.size()-1].add,remain[remain.size()-1].data,-1 );
 	
-	for (int i = 0; i+1<v.size(); ++i)
-	{
-			printf("%05d %d %05d\n", v[i].add, v[i].key, v[i+1].add);
-	}
-	if(v.size()>=1)
-	printf("%05d %d %d\n", v[v.size()-1].add, v[v.size()-1].key, -1);
-
-	for (int i = 0; i+1 <remain.size(); ++i)
-	{
-			printf("%05d %d %05d\n", remain[i].add, remain[i].key, remain[i+1].add);//这种题尽量不要改指针next,用下一个节点的address代替即可！！
-	}
-	if(remain.size()>=1)
-	printf("%05d %d %d\n", remain[remain.size()-1].add, remain[remain.size()-1].key, -1);
-	getchar();
+	for (int i = 0; i+1 < del.size(); ++i)
+		printf("%05d %d %05d\n",del[i].add,del[i].data,del[i+1].add );	
+	if(del.size()>0)//注意一定要判断del是否为空！否则段错误！
+		printf("%05d %d %d\n",del[del.size()-1].add,del[del.size()-1].data,-1 );
 	system("pause");
 	return 0;
 }
+
