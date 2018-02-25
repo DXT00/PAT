9c69374932f81b567d8d693b09bc4de7530cf283
DXT00 <dingxiaotong11@yahoo.com>
2018-02-25 12:18:19 +0800

    dfs

--- 1106 Lowest Price in Supply Chain_25.cpp
+++ 1106 Lowest Price in Supply Chain_25.cpp
@@ -1,41 +1,40 @@
-#include <iostream>
-#include <stdio.h>
 #include <vector>
 #include <algorithm>
+#include <stdio.h>
+#include <iostream>
 using namespace std;
-int N, MIN_level, k,x;
-double P, r, MIN = 10000000002.0;
-std::vector<int> level(100002, 0);
-void dfs(vector<vector<int> > &v, int root, int le, double price){
-	if (v[root].size() == 0){
-		level[le]++;
-		if (price<MIN){
-			MIN = price;
-			MIN_level = le;
+
+int N,k,x,cnt=0;
+double P,r,MIN=99999999;
+std::vector<int> cur;
+std::vector<vector<int> > v;
+void dfs(int root,double price){
+	if(v[root].size()==0){
+		if(price<MIN){
+			MIN=price;
+			cnt=1;
 		}
-		return;
+		else if(price==MIN){
+			cnt++;
+		}
 	}
 	for (int i = 0; i <v[root].size(); ++i)
-		dfs(v, v[root][i], le + 1, price*(1 + 0.01*r));
+		dfs(v[root][i],price*(1+0.01*r));
+	
 }
 int main(int argc, char const *argv[])
 {
-	scanf("%d%lf%lf", &N, &P, &r);
-	std::vector<int> cur;
-	std::vector<vector<int> > v(N, cur);
-	for (int i = 0; i <N; ++i)
-	{
-		scanf("%d", &k);
-		for (int j = 0; j <k; ++j)
-		{
-			scanf("%d", &x);
+	scanf("%d%lf%lf",&N,&P,&r);
+	v.assign(N,cur);
+	for (int i = 0; i <N; ++i){
+		scanf("%d",&k);
+		for (int j = 0; j <k; ++j){
+			scanf("%d",&x);
 			v[i].push_back(x);
 		}
 	}
-
-	dfs(v, 0, 0, P);
-	printf("%.4lf %d\n", MIN, level[MIN_level]);
-	getchar();
+	dfs(0,P);
+	printf("%.4lf %d\n",MIN,cnt);
 	system("pause");
 	return 0;
 }
