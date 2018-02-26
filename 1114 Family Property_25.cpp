881d51191c11536f46aebc7ec1c50c17172eb5bf
DXT00 <dingxiaotong11@yahoo.com>
2018-02-26 21:10:26 +0800

    并查集~好题再做3~

--- 1114 Family Property_25.cpp
+++ 1114 Family Property_25.cpp
@@ -79,3 +79,100 @@ int main(int argc, char const *argv[])
 	system("pause");
 	return 0;
 }
+
+/*
+//第二次做的：
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#include <set>
+using namespace std;
+int N, me, father, mother, child, k,num1,total1;
+int v[10000], total[10000], num[10000];
+std::vector<int> res;
+set<int> s;
+int findRoot(int x){
+	while (v[x] >= 0){
+		x = v[x];
+	}
+	return x;
+}
+void Union(int a, int b){
+	int roota = findRoot(a);
+	int rootb = findRoot(b);
+	
+	if (roota != rootb){
+		if (rootb<roota)
+			swap(roota, rootb);
+
+		v[roota] += v[rootb];
+		v[rootb] = roota;
+
+		total[roota] += total[rootb];
+		total[rootb] = 0;
+
+		num[roota] += num[rootb];
+		num[rootb] = 0;
+
+	}
+
+}
+bool cmp(int a, int b){
+	if ((double)total[a] / (-v[a]) != (double)total[b] / (-v[b]))
+		return (double)total[a] / (-v[a])>(double)total[b] / (-v[b]);
+	else
+		return a<b;
+}
+int main(int argc, char const *argv[])
+{
+	scanf("%d", &N);
+	fill(v, v + 10000, -1);
+	for (int i = 0; i <N; ++i)
+	{
+		scanf("%d%d%d%d", &me, &father, &mother, &k);
+		s.insert(me);
+		vector<int> children(k);
+		for (int i = 0; i <k; ++i){
+			scanf("%d", &children[i]);
+		}
+		scanf("%d%d", &num1, &total1);
+
+		if (father != -1){
+			Union(me, father);
+			s.insert(father);
+		}
+		if (mother != -1){
+			Union(me, mother);
+			s.insert(mother);
+		}
+		for (int i = 0; i <k; ++i){
+
+			Union(me, children[i]);
+			s.insert(children[i]);
+		}
+		int root = findRoot(me);//一行是一个家庭，重新找这个家庭的root,即最小i
+		total[root] += total1;//把total1加进去
+		num[root] += num1;//把num1加进去
+	}
+	set<int>::iterator it;
+	for (it = s.begin(); it != s.end(); it++)//为了不遍历10000个数-->用了set
+	{
+		if (v[*it]<0){
+			res.push_back(*it);
+		}
+	}
+	printf("%d\n", res.size());
+	sort(res.begin(), res.end(), cmp);
+	for (int i = 0; i <res.size(); ++i)
+	{
+		
+		printf("%04d %d %.3lf %.3lf\n", res[i], -v[res[i]], (double)num[res[i]] / (-v[res[i]]), (double)total[res[i]] / (-v[res[i]]));
+	}
+	system("pause");
+	return 0;
+}
+
+
+
+*/
