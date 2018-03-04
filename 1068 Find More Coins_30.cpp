83a5cf8859dede469483ca853ef957c452082900
DXT00 <dingxiaotong11@yahoo.com>
2018-03-04 19:25:11 +0800

    动态规划~也可以用dfs

--- 1068 Find More Coins_30.cpp
+++ 1068 Find More Coins_30.cpp
@@ -9,61 +9,70 @@ using namespace std;
 /*
 
 //dfs超时了==！！
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+std::vector<int>res;
 int N, M;
-bool dfs(vector<int> &v, int i, vector<int> &s, int &sum){
-	bool flag = false;
-	sum += v[i];
-	s.push_back(v[i]);
-	if (sum>M)return false;
-	else if (sum == M)return true;
+std::vector<int> v;
 
-	for (int j = i + 1; j <N; ++j)
-	{
-		flag = dfs(v, j, s, sum);
-		if (flag)break;
-		sum -= v[j];
-		s.pop_back();
-		if(flag==false&&sum+v[j]>M)break;
-	}
-	return flag;
+bool cmp(vector<int>a,vector<int>b){
+	return a<b;
 }
-void find(vector<int> &v){
-	vector<int> s;
-	bool flag = false;
-	int sum = 0;
-	for (int i = 0; i<N; i++){
-		if(v[i]>M) break;
-		sum = 0;
-		flag = dfs(v, i, s, sum);
-		if (flag)break;
-		
-	}
-	if(flag==false){
-		cout<<"No Solution"<<endl;
+bool flag=false;
+void dfs(int x,int sum,vector<int>path){
+	if(sum>M)return;
+	if(sum==M){
+		res=path;
+		flag=true;
 		return;
 	}
-	for (int i = 0; i <s.size(); ++i)
+	for (int i = x+1; i <N; ++i)
 	{
-		if(i!=s.size()-1)
-			printf("%d ",s[i]);
-		else
-			printf("%d\n",s[i]);
+		path.push_back(v[i]);
+		dfs(i,sum+v[i],path);
+		if(flag)return;
+		path.pop_back();
 	}
 }
 int main(int argc, char const *argv[])
 {
 	scanf("%d%d", &N, &M);
-	std::vector<int> v(N);
+	v.assign(N,0);
 	for (int i = 0; i <N; ++i)
 	{
 		scanf("%d", &v[i]);
 	}
 	sort(v.begin(), v.end());
-	find(v);
+
+	vector<int>path;
+	for (int i = 0; i <v.size(); ++i)
+	{
+		if(v[i]>M)break;
+		path.push_back(v[i]);
+		dfs(i,v[i],path);
+		if(flag)break;
+		path.pop_back();
+	}
+	if(res.size()==0){
+		printf("No Solution\n");
+	}
+	else{
+		for (int i = 0; i <res.size(); ++i)
+		{
+			if(i!=0)printf(" ");
+			printf("%d",res[i]);
+		}
+	}
+	
+
 	getchar();
 	system("pause");
 	return 0;
 }
+
 */
 int N,M;
 void find(std::vector<int> &v){
